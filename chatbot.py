36bffbeb739e8929e5ac8b13cc017beae433db55
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2023-06-25 22:53:54 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,7 +13,7 @@ import time
 st.set_page_config(menu_items=None,page_title="Inflow Chatbot", page_icon="path/to/your/favicon.ico", initial_sidebar_state="collapsed")
 
 
-with open('Style.css') as f:
+with open('C:\Users\z_sal\OneDrive\Desktop\My Chatbot app\Chatbot-inflow\Style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
 
